041fd0a55d2a2a1281224433b41cac59b20760b0
Carles <cjuan@apsl.net>
2021-10-21 09:31:36 +0200

    First commit. Empty screen

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![allow(unused)] // Temporal
+
+use bevy::prelude::*;
+
+const PLAYER_SPRITE: &str = "player_a_01.png";
+
+fn main() {
+    App::build()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor{
+            title: "Rust Invaders!".to_string(),
+            width: 598.0,
+            height: 676.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
+}
