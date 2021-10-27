63ca29b262988b1d830794f64ec7fb9155bb702a
Carles <cjuan@apsl.net>
2021-10-27 15:44:13 +0200

    rectangle added

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,27 @@ use bevy::prelude::*;
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
 
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut windows: ResMut<Windows>
+) {
+    // camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // position window
+    let window = windows.get_primary_mut().unwrap();
+    window.set_position(IVec2::new(3870, 4830));
+
+    // spawn sprite
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.add(Color::rgb(1., 0.7, 0.7).into()),
+            sprite: Sprite::new(Vec2::new(200.0, 100.0)),
+            ..Default::default()
+        });
+}
+
 fn main() {
     App::build()
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
@@ -16,17 +37,4 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .run();
-}
-
-fn setup(
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut windows: ResMut<Windows>
-) {
-    // camera
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-
-    // position window
-    let window = windows.get_primary_mut().unwrap();
-    window.set_position(IVec2::new(3870, 4830));
 }
