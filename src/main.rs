82aab54afc06376757dac101f9ab2d0c299c5de0
Carles <cjuan@apsl.net>
2021-10-28 12:45:12 +0200

    refactor to multiple bevy systems

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,27 @@ use bevy::prelude::*;
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
 
-fn setup(
+// Resources
+pub struct Materials {
+    player_materials: Handle<ColorMaterial>,
+}
+
+struct WinSize {
+    w: f32,
+    h: f32,
+}
+
+// Systems
+fn player_spawn(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut windows: ResMut<Windows>
+    mut materials: Res<Materials>,
+    win_size: Res<WinSize>,
 ) {
-    // camera
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-
-    // position window
-    let window = windows.get_primary_mut().unwrap();
-    window.set_position(IVec2::new(3870, 4830));
-
     // spawn sprite
-    let bottom = - window.height() / 2.;
+    let bottom = - win_size.h / 2.;
     commands
         .spawn_bundle(SpriteBundle {
-            material: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+            material: materials.player_materials.clone(),
             transform: Transform {
                 translation: Vec3::new(0., bottom + 75. / 4. + 5., 10.),
                 scale: Vec3::new(0.5, 0.5, 1.),
@@ -29,6 +32,31 @@ fn setup(
             },
             ..Default::default()
         });
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut windows: ResMut<Windows>
+) {
+    let window = windows.get_primary_mut().unwrap();
+
+    // camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // create main resources
+    commands.insert_resource(Materials {
+        player_materials: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+    });
+
+    commands.insert_resource(WinSize {
+        w: window.width(),
+        h: window.height(),
+    });
+
+    // position window
+    window.set_position(IVec2::new(3870, 4830));
 }
 
 fn main() {
@@ -42,5 +70,6 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_startup_stage("game_setup_actors", SystemStage::single(player_spawn.system()))
         .run();
 }
