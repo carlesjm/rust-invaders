36079a4e2b7c3439d185001967db01f922544b60
Carles <cjuan@apsl.net>
2021-10-29 14:29:04 +0200

    spawn laser

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@
 use bevy::prelude::*;
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
+const LASER_SPRITE: &str = "laser_a_01.png";
 const TIME_STEP: f32 = 1. / 60.;
 
 // Resources
 pub struct Materials {
     player_materials: Handle<ColorMaterial>,
+    laser_materials: Handle<ColorMaterial>,
 }
 
 struct WinSize {
@@ -17,8 +19,9 @@ struct WinSize {
 
 // Components
 struct Player;
-struct PlayerSpeed(f32);
-impl Default for PlayerSpeed {
+struct Laser;
+struct Speed(f32);
+impl Default for Speed {
     fn default() -> Self {
         Self(500.)
     }
@@ -43,12 +46,12 @@ fn player_spawn(
             ..Default::default()
         })
         .insert(Player)
-        .insert(PlayerSpeed::default());
+        .insert(Speed::default());
 }
 
 fn player_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&PlayerSpeed, &mut Transform), With<Player>>
+    mut query: Query<(&Speed, &mut Transform), With<Player>>
 ) {
     if let Ok((speed, mut transform)) = query.single_mut() {
         let dir = if keyboard_input.pressed(KeyCode::Left) {
@@ -59,6 +62,31 @@ fn player_movement(
             0.
         };
         transform.translation.x += dir * speed.0 * TIME_STEP;
+    }
+}
+
+fn player_fire(
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut materials: Res<Materials>,
+    query: Query<(&Transform), With<Player>>
+) {
+    if let Ok((player_transform)) = query.single() {
+        if keyboard_input.pressed(KeyCode::Space) {
+            let x = player_transform.translation.x;
+            let y = player_transform.translation.y;
+
+            commands.spawn_bundle(SpriteBundle {
+                material: materials.laser_materials.clone(),
+                transform: Transform {
+                    translation: Vec3::new(x, y, 0.),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(Laser)
+            .insert(Speed);
+        }
     }
 }
 
@@ -76,6 +104,7 @@ fn setup(
     // create main resources
     commands.insert_resource(Materials {
         player_materials: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+        laser_materials: materials.add(asset_server.load(LASER_SPRITE).into()),
     });
 
     commands.insert_resource(WinSize {
@@ -100,5 +129,6 @@ fn main() {
         .add_startup_system(setup.system())
         .add_startup_stage("game_setup_actors", SystemStage::single(player_spawn.system()))
         .add_system(player_movement.system())
+        .add_system(player_fire.system())
         .run();
 }
